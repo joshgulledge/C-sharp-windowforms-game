faf9ebd5ba90e6f4281b2248915af72a019ea37e
Joshua Gulledge <jgulledge@ddmsinc.com>
2021-07-29 14:49:05 -0500

    changed constructor methods and added properties

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -1,4 +1,5 @@
 ﻿using Engine;
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,12 +22,7 @@ namespace SuperAdventure
 
             Location location = new Location(1, "Home", "This is your house.");
 
-            _player = new Player ();
-            _player.CurrentHitPoints = 10;
-            _player.MaximumHitPoints = 10;
-            _player.Gold = 20;
-            _player.ExperiencePoints = 0;
-            _player.Level = 1;
+            _player = new Player(10, 10, 20, 0, 1);
 
             HitPointsDisplay.Text = _player.CurrentHitPoints.ToString();
             GoldDisplay.Text = _player.Gold.ToString();
